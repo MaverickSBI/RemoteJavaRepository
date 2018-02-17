ad51a779b7eda169a16b2e753448b07a4b5c4273
Jignesh Sadiya <akash4ever1996@gmail.com>
2018-02-17 09:38:41 +0530

    intial commit by Jignesh Sadiya

---
+++ PJavaPractice/src/com/allianz/absi/person/A.java
@@ -0,0 +1,25 @@
+package com.allianz.absi.person;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Stack stack= new Stack();
+
+		Queue q=  new LinkedList<>();
+		Queue q1= new PriorityQueue<>();
+		
+		
+		Map m = new HashMap();
+	}
+
+}
