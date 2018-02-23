37beef61781f7192b6dbca51993a7cddfafd3863
Jignesh Sadiya <akash4ever1996@gmail.com>
2018-02-23 19:18:47 +0530

    Few Codes Changed

---
+++ PJavaPractice/src/com/allianz/absi/person/BestPractice.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.allianz.absi.person;
+
+/**
+ * @author Jignesh Sadiya
+ *
+ */
+public class BestPractice {
+	
+	private class inner{
+		public inner() {
+			super();
+		}
+		
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
